928e43f8dd46a5aa6e8fdc3b0c990f6aaae5490e
rhao22-netizen <rhao22@coe.edu>
2026-01-29 17:20:44 -0600

    Implement service worker for caching assets

--- sw-3.js
+++ sw-3.js
@@ -1,1 +1,24 @@
-keep
+const CACHE = "remesur-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
